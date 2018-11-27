efaf3f02ef6aa2463a3420818dce568f9c2f08b3
Christopher Boysen <christopher.boysen@smail.inf.h-brs.de>
2018-11-27 06:21:16 +0100

    next steps

--- AndroidprojectNXT/src/Main.java
+++ AndroidprojectNXT/src/Main.java
@@ -3,6 +3,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
@@ -16,6 +17,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		bluetoothConnection = Bluetooth.waitForConnection();
+		LCD.drawString("Verbunden", 1, 1);
 		bluetoothConnection.setIOMode(NXTConnection.RAW);
 		dataOutputStream = bluetoothConnection.openDataOutputStream();
 		dataInputStream = bluetoothConnection.openDataInputStream();
@@ -23,8 +25,11 @@ public class Main {
 		while(true) {
 			// Qualifikationsfahrt
 			if(receive() == 99) {
-				CompassControl compass = new CompassControl();
-				compass.calibrateDegree();
+				LCD.clear();
+				LCD.drawString("Quali", 1, 1);
+//				send(99);
+//				CompassControl compass = new CompassControl();
+//				compass.calibrateDegree();
 				Motor.B.setSpeed(720);
 				Motor.C.setSpeed(720);
 				Motor.B.forward();
@@ -32,47 +37,51 @@ public class Main {
 				Thread.sleep(1000);
 				Motor.B.stop();
 				Motor.C.stop();
-				while(compass.getDegrees() < 90) {
+/*				while(compass.getDegrees() < 90) {
 					Motor.C.forward();
 				}
+*/
 				Motor.C.stop();
 				Motor.B.forward();
 				Motor.C.forward();
 				Thread.sleep(500);
 				Motor.B.stop();
 				Motor.C.stop();
-				compass.calibrateDegree();
-				while(compass.getDegrees() < 90) {
+//				compass.calibrateDegree();
+/*				while(compass.getDegrees() < 90) {
 					Motor.C.forward();
 				}
-				Motor.C.stop();
+*/				Motor.C.stop();
 				Motor.B.forward();
 				Motor.C.forward();
 				Thread.sleep(1000);
 				Motor.B.stop();
 				Motor.C.stop();
-				compass.calibrateDegree();
+/*				compass.calibrateDegree();
 				while(compass.getDegrees() < 90) {
 					Motor.C.forward();
 				}
-				Motor.C.stop();
+*/				Motor.C.stop();
 				Motor.B.forward();
 				Motor.C.forward();
 				Thread.sleep(500);
 				Motor.B.stop();
 				Motor.C.stop();
-				compass.calibrateDegree();
+/*				compass.calibrateDegree();
 				while(compass.getDegrees() < 90) {
 					Motor.C.forward();
 				}
-				Motor.C.stop();
-				send(0);
+*/				Motor.C.stop();
+				LCD.clear();
+				LCD.drawString("Quali Bestanden", 1, 1);
+//				send(98);
 			}
 			// Disconnect
-			if(receive() == 100) {
+			if(receive() == 50) {
 				break;
 			}
 		}
+
 		disconnect();
 	}
 	
